7cf9ee72fe0dee20afe34a376f236e79b58f2af5
Everton Luis <evertonluissantossilva@outlook.com>
2026-09-10 21:20:27 -0300

    variáveis e tipos de dados js

---
+++ javascript/aula01-introducao/main.js
@@ -0,0 +1,9 @@
+const firstName = "Everton";
+const lastName = "Silva";
+
+const names = "Felipe, João, Julia";
+
+console.log("Meu nome é " + firstName + " " + lastName);
+console.log(`Meu nome é ${firstName} ${lastName}`);
+
+console.log(names.split(","));
